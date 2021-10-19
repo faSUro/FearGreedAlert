0ec95233a172a530eb93d50b11d900b51e61f3dc
Nicolò Fasulo <fasulo.nicol@gmail.com>
2021-10-19 20:42:36 +0200

    Levels and general plan added

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+# Threshold to decide whether to send the email or not
+EXTREME_FEAR_LEVEL = 25
+FEAR_LEVEL = 45
+GREED_LEVEL = 55
+EXTREME_GREED_LEVEL = 75
 
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# API Call (alternative.me)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Threshold evaluation
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Sending emails...
+
+
