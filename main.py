1b93dc24522d71299bb8fbbb7804c3e130346444
Nicolò Fasulo <fasulo.nicol@gmail.com>
2021-10-21 09:48:38 +0200

    added message building. done

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import requests
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -9,16 +10,21 @@ FEAR_LEVEL = 45
 GREED_LEVEL = 55
 EXTREME_GREED_LEVEL = 75
 
+# Suggestions depending on the index
+suggestion = {'EXTREME FEAR': "sell your house and buy this shit, seriously",
+              'FEAR': "start buying by bartering some of your possessions, not all of them",
+              'GREED': "start selling, things are getting hot",
+              'EXTREME GREED': "sell all your coins before this bubble pops and a stallion jams his dick in your ass"}
+
 
 def retrieve_mail_bot_credentials():
-    bot_email = ""
-    bot_password = ""
-    return [bot_email, bot_password]
+    with open('mail bot credentials.txt', 'r') as c:
+        return c.read().splitlines()
 
 
 def retrieve_mailing_list():
-    recipients = ["", ""]
-    return recipients
+    with open('mailing list.txt', 'r') as c:
+        return c.read().splitlines()
 
 
 def send_alert_mail(msg_body):
@@ -27,11 +33,11 @@ def send_alert_mail(msg_body):
 
     msg = MIMEMultipart()
 
-    msg['From'] = ""  # retrieve from mail bot credentials
+    msg['From'] = credentials[0]  # retrieve from mail bot credentials
     msg['To'] = msg['To'] = ", ".join(recipients)
     msg['Subject'] = 'Fear & Greed Index Alert'
 
-    msg.attach(MIMEText(msg_body, 'plain'))
+    msg.attach(MIMEText(msg_body, 'html'))
 
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.ehlo()
@@ -41,37 +47,44 @@ def send_alert_mail(msg_body):
 
 
 try:
-    # API Call (alternative.me)
+    # Get Fear & Greed Index (alternative.me API)
     API = "https://api.alternative.me/fng/"
     response = requests.get(API)
     # json response sample:
     # {'name': '', 'data': [{'value': '', 'value_classification': '', 'timestamp': '', 'time_until_update': ''}],
     # 'metadata': {'error': ''}}
-    index = response.json()['data'][0]['value']
+    index = int(response.json()['data'][0]['value'])
 
-    # Get BTC price (Coinbase)
+    # Get BTC price (Coinbase API)
     API = "https://api.coinbase.com/v2/prices/BTC-USD/spot"
     response = requests.get(API)
     # json response sample:
     # {"data":{"base":"","currency":"","amount":""}}
-    price = response.json()['data']['amount']
+    price = float(response.json()['data']['amount'])
+
+    # Threshold evaluation and message construction
+    level = ""
+    if index < EXTREME_FEAR_LEVEL:
+        level = 'EXTREME FEAR'
+    elif index < FEAR_LEVEL:
+        level = 'FEAR'
+    elif index > EXTREME_GREED_LEVEL:
+        level = 'GREED'
+    elif index > GREED_LEVEL:
+        level = 'EXTREME GREED'
+    else:
+        exit(0)  # Index is in neutral range, no email has to be sent
+
+    # Building the message
+    message = f"""<html><head></head><body>The Fear & Greed Index is <b>{index}</b>, corresponding to <b>{level}</b
+    >.<br> The price of BTC is <b>${price}</b>.<br> 
+    My suggestion is to {suggestion[level]}.</body></html><br><br>
+    Yours faithfully,<br>
+    FeaGA Bot"""
+
+    # Sending emails...
+    send_alert_mail(message)
 except Exception as e:
     with open('errorLog.txt', 'a') as f:
-        f.write(e + '\n')
+        f.write(str(datetime.now()) + ': ' + str(e) + '\n')
     exit(-1)
-exit(0)
-# Threshold evaluation and message construction
-message = ""  # TODO
-if index < EXTREME_FEAR_LEVEL:
-    send_alert_mail(index)
-elif index < FEAR_LEVEL:
-    send_alert_mail(index)
-elif index > EXTREME_GREED_LEVEL:
-    send_alert_mail(index)
-elif index > GREED_LEVEL:
-    send_alert_mail(index)
-else:
-    exit(0)  # Index is in neutral range, no email has to be sent
-
-# Sending emails...
-send_alert_mail(message)
